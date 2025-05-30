75f74da3e8f2fff3cc7afecccf1aaf543719ebfe
Abed <abd.srour313@gmail.com>
2025-05-30 13:42:23 +0300

    Add test route to complaintRoutes for debugging

--- routes/complaintRoutes.js
+++ routes/complaintRoutes.js
@@ -11,5 +11,9 @@ router.post('/', upload.array('photos', 10), submitComplaint);
 router.get('/', getAllComplaints);
 router.put('/:id/status', updateStatus);
 router.get('/hidden', getHiddenComplaints);
+router.get('/test', (req, res) => {
+  res.json({ message: 'Complaint route is active' });
+});
+
 
 module.exports = router;
